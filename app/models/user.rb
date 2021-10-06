267b7e225a778e2c7fc3d731b90ab53e5ab611a5
elmo0509 <elmo.boof@gmail.com>
2021-10-06 00:52:04 +0000

    [Finish]relationshipモデルの記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,12 @@ class User < ApplicationRecord
   
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  
+  has_many :relationships, foreign_key: :follower_id
+  has_many :follower, through: :relationships, source: :followed
+  
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :followed_id
+  has_many :followed, through: :reverse_of_relationships, source: :follower
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
