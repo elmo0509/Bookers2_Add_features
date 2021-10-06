5fbaaabfa4ad4c3482be8441d1559101782f1bfe
elmo0509 <elmo.boof@gmail.com>
2021-10-06 01:00:09 +0000

    relationships_contoroller/アクション追加

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,13 @@
+class RelationshipsController < ApplicationController
+  def create
+    follower = current_user.relationships.build(follower_id: params[:user_id])
+    follower.save
+    redirect_to request.referrer
+  end
+  
+  def destroy
+    follower = current_user.relationships.find_by(follower_id: params[:user_id])
+    follower.destroy
+    redirect_to request.referrer
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users,only: [:show,:index,:edit,:update]
+  
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only:[:create, :destroy]
+    get :follower, on: :member
+    get :followed, on: :member
+  end
+  
   resources :books do
     resource :favorites, only:[:create, :destroy]
     resources :book_comments, only:[:create, :destroy]
