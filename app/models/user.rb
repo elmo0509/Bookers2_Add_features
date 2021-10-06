846cb7bc09c8633ae2415b98fed3dd4b3f325789
elmo0509 <elmo.boof@gmail.com>
2021-10-06 02:30:27 +0000

    followed/follower.html作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,19 @@ class User < ApplicationRecord
 
   has_many :books
   attachment :profile_image, destroy: false
-  
+
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
+
   has_many :relationships, foreign_key: :follower_id
   has_many :follower, through: :relationships, source: :followed
-  
+
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :followed_id
   has_many :followed, through: :reverse_of_relationships, source: :follower
+
+  def is_follower_by?(user)
+    reverse_of_relationships.find_by(follower_id: user.id).present?
+  end
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
