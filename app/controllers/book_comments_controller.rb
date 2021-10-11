943021cd56efb060105ab926186d3448251ba26a
elmo0509 <elmo.boof@gmail.com>
2021-10-11 18:14:40 +0000

    コメント機能の非同期

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,20 +1,23 @@
 class BookCommentsController < ApplicationController
-  
+
+  # 保存したらindex.js.erbを表示するように設定
   def create
-    book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to request.referrer
+    render :index
   end
-  
+
   def destroy
+    @book = Book.find(params[:book_id])
     BookComment.find_by(id: params[:id]).destroy
-    redirect_to request.referrer
+    render :index
   end
-  
+
   private
-  
+
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
